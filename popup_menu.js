9863a979f331bfcdf9688cf564f30f39f246530b
Chris <hakizachrist1@gmail.com>
2023-01-03 12:44:47 +0200

    Update js

--- popup_menu.js
+++ popup_menu.js
@@ -1,18 +1,18 @@
-const cancel = document.querySelector("#cancel");
-const hamburger = document.querySelector("#hamburger");
+const cancel = document.querySelector('#cancel');
+const hamburger = document.querySelector('#hamburger');
 
 function openDiv() {
-  let popup = document.querySelector("#popup");
-  popup.style.display = "block";
+  let popup = document.querySelector('#popup');
+  popup.style.display = 'block';
 }
 
 function closeDiv() {
-  let get = document.querySelector("#popup");
-  get.style.display = "none";
+  let get = document.querySelector('#popup');
+  get.style.display = 'none';
 }
 
-cancel.addEventListener("click", function () {
+cancel.addEventListener('click', function () {
   closeDiv();
 });
 
-hamburger.addEventListener("click", openDiv);
+hamburger.addEventListener('click', openDiv);
