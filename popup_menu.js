6f0deabf346901021fdd4fd9838be93dde347ab2
Chris <hakizachrist1@gmail.com>
2023-01-05 12:38:49 +0200

    Fix linter

--- popup_menu.js
+++ popup_menu.js
@@ -34,73 +34,63 @@ option3.addEventListener('click', () => {
   closeDiv();
 });
 
-
 // Detail popup window start here. 
-
-var arr = [
+let arr = [
  {
-  id: '1',
-  Project_name: 'TONIC',
-  Featured_image: 'images/Snapshoot_Portfolio .png',
-  Feature_image_desk: 'images/Snapshoot_Portfolio_(4).png',
-  Featured: ['CANOPY','Back End Dev','2015'],
-  Project_descr: [' A daily selection of privately personalized reads; no accounts or sign-ups required.', 'Exploring the future of media in Facebook s first Virtual Reality app; a place to discover and enjoy 360 photos and videos on Gear VR.'],
-  technologies:['html','javascript','css'],
-
-  //popup_details
-  Project_descr_pop: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent',
-  link_to_live_version: 'https://hakichris.github.io/Christian_Portfolio.github.io/',
-  Source: 'https://github.com/hakichris/Christian_Portfolio.github.io.git',
+  id:  '1',
+  Project_name:  'TONIC',
+  Featured_image:  'images/Snapshoot_Portfolio .png',
+  Feature_image_desk:  'images/Snapshoot_Portfolio_(4).png',
+  Featured:  ['CANOPY','Back End Dev','2015'],
+  Project_descr:  [' A daily selection of privately personalized reads; no accounts or sign-ups required.', 'Exploring the future of media in Facebook s first Virtual Reality app; a place to discover and enjoy 360 photos and videos on Gear VR.'],
+  technologies:  ['html','javascript','css'],
+  Project_descr_pop:  'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent',
+  link_to_live_version:  'https://hakichris.github.io/Christian_Portfolio.github.io/',
+  Source:  'https://github.com/hakichris/Christian_Portfolio.github.io.git',
 },
 
 {
-  id:'2',
-  Project_name: 'Multi-Post Stories',
-  Featured_image: 'images/Snapshoot_Portfolio_(1).png',
-  Feature_image_desk: 'images/Snapshoot_Portfolio_(5).png',
-  Featured: ['CANOPY','Back End Dev','2015','FACEBOOK','Full Stack Dev',],
-  image: ['<img src="images/Snapshoot Portfolio .png" alt=" Project snapshoot" />','images/Snapshoot Portfolio (4).png','images/Counter,png'],
-  Project_descr: [' A daily selection of privately personalized reads; no accounts or sign-ups required.', 'Exploring the future of media in Facebook s first Virtual Reality app; a place to discover and enjoy 360 photos and videos on Gear VR.'],
-  technologies:['html','javascript','css', 'Ruby on rails'],
-
-  //popup_details
-  Project_descr_pop: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent',
-  link_to_live_version: 'https://hakichris.github.io/Christian_Portfolio.github.io/',
-  Source: 'https://github.com/hakichris/Christian_Portfolio.github.io.git',
+  id:  '2',
+  Project_name:  'Multi-Post Stories',
+  Featured_image:  'images/Snapshoot_Portfolio_(1).png',
+  Feature_image_desk:  'images/Snapshoot_Portfolio_(5).png',
+  Featured:  ['CANOPY','Back End Dev','2015','FACEBOOK','Full Stack Dev',],
+  image:  ['<img src="images/Snapshoot Portfolio .png" alt=" Project snapshoot" />','images/Snapshoot Portfolio (4).png','images/Counter,png'],
+  Project_descr:  [' A daily selection of privately personalized reads; no accounts or sign-ups required.', 'Exploring the future of media in Facebook s first Virtual Reality app; a place to discover and enjoy 360 photos and videos on Gear VR.'],
+  technologies:  ['html','javascript','css', 'Ruby on rails'],
+  Project_descr_pop:  'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent',
+  link_to_live_version:  'https://hakichris.github.io/Christian_Portfolio.github.io/',
+  Source:  'https://github.com/hakichris/Christian_Portfolio.github.io.git',
 },
 
 {
-  id:'3',
-  Project_name: 'Facebook 360',
-  Featured_image: 'images/Snapshoot_Portfolio_(2).png',
-  Feature_image_desk: 'images/Snapshoot_Portfolio_(6).png',
-  Featured: ['CANOPY','Back End Dev','2015','FACEBOOK','Full Stack Dev',],
-  image: ['<img src="images/Snapshoot Portfolio .png" alt=" Project snapshoot" />','images/Snapshoot Portfolio (4).png','images/Counter,png'],
-  Project_descr: [' A daily selection of privately personalized reads; no accounts or sign-ups required.', 'Exploring the future of media in Facebook s first Virtual Reality app; a place to discover and enjoy 360 photos and videos on Gear VR.'],
-  technologies:['html','javascript','css','Ruby on rails'],
-  image: 'images/Counter.png',
-  
-  //popup_details
-  Project_descr_pop: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent',
-  link_to_live_version: 'https://hakichris.github.io/Christian_Portfolio.github.io/',
-  Source: 'https://github.com/hakichris/Christian_Portfolio.github.io.git',
+  id:  '3',
+  Project_name:  'Facebook 360',
+  Featured_image:  'images/Snapshoot_Portfolio_(2).png',
+  Feature_image_desk:  'images/Snapshoot_Portfolio_(6).png',
+  Featured:  ['CANOPY','Back End Dev','2015','FACEBOOK','Full Stack Dev',],
+  image:  ['<img src="images/Snapshoot Portfolio .png" alt=" Project snapshoot" />','images/Snapshoot Portfolio (4).png','images/Counter,png'],
+  Project_descr:  [' A daily selection of privately personalized reads; no accounts or sign-ups required.', 'Exploring the future of media in Facebook s first Virtual Reality app; a place to discover and enjoy 360 photos and videos on Gear VR.'],
+  technologies:  ['html','javascript','css','Ruby on rails'],
+  image:  'images/Counter.png',
+  Project_descr_pop:  'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent',
+  link_to_live_version:  'https://hakichris.github.io/Christian_Portfolio.github.io/',
+  Source:  'https://github.com/hakichris/Christian_Portfolio.github.io.git',
 },
 
 {
-  id:'4',
-  Project_name: 'Uber Navigation',
-  Featured_image: 'images/Snapshoot_Portfolio_(3).png',
-  Feature_image_desk: 'images/Snapshoot_Portfolio_(4).png',
-  Featured: ['CANOPY','Back End Dev','2015','Uber','Lead Developer','2018'],
-  image: ['<img src="images/Snapshoot Portfolio .png" alt=" Project snapshoot" />','images/Snapshoot Portfolio (4).png','images/Counter,png'],
-  Project_descr: [' A daily selection of privately personalized reads; no accounts or sign-ups required.',' A daily selection of privately personalized reads; no accounts orsign-ups required.'],
-  technologies:['html','javascript','css','Ruby on rails'],
-  image: 'images/Counter.png',
-  
-  //popup_details
-  Project_descr_pop: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent',
-  link_to_live_version: 'https://hakichris.github.io/Christian_Portfolio.github.io/',
-  Source: 'https://github.com/hakichris/Christian_Portfolio.github.io.git',
+  id:  '4',
+  Project_name:  'Uber Navigation',
+  Featured_image:  'images/Snapshoot_Portfolio_(3).png',
+  Feature_image_desk:  'images/Snapshoot_Portfolio_(4).png',
+  Featured:  ['CANOPY','Back End Dev','2015','Uber','Lead Developer','2018'],
+  image:  ['<img src="images/Snapshoot Portfolio .png" alt=" Project snapshoot" />','images/Snapshoot Portfolio (4).png','images/Counter,png'],
+  Project_descr:  [' A daily selection of privately personalized reads; no accounts or sign-ups required.',' A daily selection of privately personalized reads; no accounts orsign-ups required.'],
+  technologies:  ['html','javascript','css','Ruby on rails'],
+  image:  'images/Counter.png',
+  Project_descr_pop:  'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent',
+  link_to_live_version:  'https://hakichris.github.io/Christian_Portfolio.github.io/',
+  Source:  'https://github.com/hakichris/Christian_Portfolio.github.io.git',
 }
 ]
 
