41e7023b6d9ba18baa814044cb3e15269b603f98
Chris <hakizachrist1@gmail.com>
2023-01-05 22:14:37 +0200

    Update validation content

--- Validation.js
+++ Validation.js
@@ -1,21 +1,19 @@
-const formButton = document.querySelector('#button');
+const formButton = document.querySelector('.form_button');
 function validate() {
   const text = document.getElementById('mail').value;
-  const validator = /^([a-z\d-]+)@([a-z\d-]+)\.([a-z]{2,8})(\.[a-z]{2,8})?$/;
+  const validator = /^[a-z]+\@[^\s]+\.[^\s]+$/g;
   document.forms[0].onsubmit = function email(e) {
-    if (!(validator.test(text))) {
-      document.getElementById('error-message').innerHTML = 'Please use lowercase to enter valid email address!';
-      document.getElementById('error-message').style.visibility = 'visible';
+    if (!validator.test(text)) {
+      document.getElementById('error-message').innerHTML =
+        'Please use lowercase to enter valid email address!';
+      document.getElementById('error-message').style.visibility = '';
       document.getElementById('error-message').style.color = 'white';
       document.getElementById('error-message').style.background = 'red';
       document.getElementById('error-message').style.textAlign = 'center';
-      console.log("i am inside");
       e.preventDefault();
     }
   };
 }
-
 formButton.addEventListener('click', () => {
   validate();
-  console.log("I passed this")
 });
