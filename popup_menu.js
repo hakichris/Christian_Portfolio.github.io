b111e5911fab99419e19a75302b021e647fec9af
Chris <hakizachrist1@gmail.com>
2023-01-05 14:35:55 +0200

    Fix linters

--- popup_menu.js
+++ popup_menu.js
@@ -1,186 +1,186 @@
-const cancel = document.querySelector("#cancel");
-const hamburger = document.querySelector("#hamburger");
-const option1 = document.querySelector(".port");
-const option2 = document.querySelector(".Ab");
-const option3 = document.querySelector(".cont");
+const cancel = document.querySelector('#cancel');
+const hamburger = document.querySelector('#hamburger');
+const option1 = document.querySelector('.port');
+const option2 = document.querySelector('.Ab');
+const option3 = document.querySelector('.cont');
 
 function openDiv() {
-  const popup = document.querySelector("#popup");
-  popup.style.display = "block";
+  const popup = document.querySelector('#popup');
+  popup.style.display = 'block';
 }
 
 function closeDiv() {
-  const get = document.querySelector("#popup");
-  get.style.display = "none";
+  const get = document.querySelector('#popup');
+  get.style.display = 'none';
 }
 
-cancel.addEventListener("click", () => {
+cancel.addEventListener('click', () => {
   closeDiv();
 });
 
-hamburger.addEventListener("click", () => {
+hamburger.addEventListener('click', () => {
   openDiv();
 });
 
-option1.addEventListener("click", () => {
+option1.addEventListener('click', () => {
   closeDiv();
 });
 
-option2.addEventListener("click", () => {
+option2.addEventListener('click', () => {
   closeDiv();
 });
 
-option3.addEventListener("click", () => {
+option3.addEventListener('click', () => {
   closeDiv();
 });
 
 const arr = [
   {
-    id: "1",
-    Project_name: "TONIC",
-    Featured_image: "images/Snapshoot_Portfolio .png",
-    Feature_image_desk: "images/Snapshoot_Portfolio_(4).png",
-    Featured: ["CANOPY", "Back End Dev", "2015"],
+    id: '1',
+    Project_name: 'TONIC',
+    Featured_image: 'images/Snapshoot_Portfolio .png',
+    Feature_image_desk: 'images/Snapshoot_Portfolio_(4).png',
+    Featured: ['CANOPY', 'Back End Dev', '2015'],
     Project_descr: [
-      " A daily selection of privately personalized reads; no accounts or sign-ups required.",
-      "Exploring the future of media in Facebook s first Virtual Reality app; a place to discover and enjoy 360 photos and videos on Gear VR.",
+      ' A daily selection of privately personalized reads; no accounts or sign-ups required.',
+      'Exploring the future of media in Facebook s first Virtual Reality app; a place to discover and enjoy 360 photos and videos on Gear VR.',
     ],
-    technologies: ["html", "javascript", "css"],
+    technologies: ['html', 'javascript', 'css'],
     Project_descr_pop:
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent",
+      'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent',
     link_to_live_version:
-      "https://hakichris.github.io/Christian_Portfolio.github.io/",
-    Source: "https://github.com/hakichris/Christian_Portfolio.github.io.git",
+      'https://hakichris.github.io/Christian_Portfolio.github.io/',
+    Source: 'https://github.com/hakichris/Christian_Portfolio.github.io.git',
   },
 
   {
-    id: "2",
-    Project_name: "Multi-Post Stories",
-    Featured_image: "images/Snapshoot_Portfolio_(1).png",
-    Feature_image_desk: "images/Snapshoot_Portfolio_(5).png",
-    Featured: ["CANOPY", "Back End Dev", "2015", "FACEBOOK", "Full Stack Dev"],
+    id: '2',
+    Project_name: 'Multi-Post Stories',
+    Featured_image: 'images/Snapshoot_Portfolio_(1).png',
+    Feature_image_desk: 'images/Snapshoot_Portfolio_(5).png',
+    Featured: ['CANOPY', 'Back End Dev', '2015', 'FACEBOOK', 'Full Stack Dev'],
     image: [
       '<img src="images/Snapshoot Portfolio .png" alt=" Project snapshoot" />',
-      "images/Snapshoot Portfolio (4).png",
-      "images/Counter,png",
+      'images/Snapshoot Portfolio (4).png',
+      'images/Counter,png',
     ],
     Project_descr: [
-      " A daily selection of privately personalized reads; no accounts or sign-ups required.",
-      "Exploring the future of media in Facebook s first Virtual Reality app; a place to discover and enjoy 360 photos and videos on Gear VR.",
+      ' A daily selection of privately personalized reads; no accounts or sign-ups required.',
+      'Exploring the future of media in Facebook s first Virtual Reality app; a place to discover and enjoy 360 photos and videos on Gear VR.',
     ],
-    technologies: ["html", "javascript", "css", "Ruby on rails"],
+    technologies: ['html', 'javascript', 'css', 'Ruby on rails'],
     Project_descr_pop:
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent",
+      'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent',
     link_to_live_version:
-      "https://hakichris.github.io/Christian_Portfolio.github.io/",
-    Source: "https://github.com/hakichris/Christian_Portfolio.github.io.git",
+      'https://hakichris.github.io/Christian_Portfolio.github.io/',
+    Source: 'https://github.com/hakichris/Christian_Portfolio.github.io.git',
   },
 
   {
-    id: "3",
-    Project_name: "Facebook 360",
-    Featured_image: "images/Snapshoot_Portfolio_(2).png",
-    Feature_image_desk: "images/Snapshoot_Portfolio_(6).png",
-    Featured: ["CANOPY", "Back End Dev", "2015", "FACEBOOK", "Full Stack Dev"],
+    id: '3',
+    Project_name: 'Facebook 360',
+    Featured_image: 'images/Snapshoot_Portfolio_(2).png',
+    Feature_image_desk: 'images/Snapshoot_Portfolio_(6).png',
+    Featured: ['CANOPY', 'Back End Dev', '2015', 'FACEBOOK', 'Full Stack Dev'],
     image: [
       '<img src="images/Snapshoot Portfolio .png" alt=" Project snapshoot" />',
-      "images/Snapshoot Portfolio (4).png",
-      "images/Counter,png",
+      'images/Snapshoot Portfolio (4).png',
+      'images/Counter,png',
     ],
     Project_descr: [
-      " A daily selection of privately personalized reads; no accounts or sign-ups required.",
-      "Exploring the future of media in Facebook s first Virtual Reality app; a place to discover and enjoy 360 photos and videos on Gear VR.",
+      ' A daily selection of privately personalized reads; no accounts or sign-ups required.',
+      'Exploring the future of media in Facebook s first Virtual Reality app; a place to discover and enjoy 360 photos and videos on Gear VR.',
     ],
-    technologies: ["html", "javascript", "css", "Ruby on rails"],
-    image: "images/Counter.png",
+    technologies: ['html', 'javascript', 'css', 'Ruby on rails'],
+    image: 'images/Counter.png',
     Project_descr_pop:
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent",
+      'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent',
     link_to_live_version:
-      "https://hakichris.github.io/Christian_Portfolio.github.io/",
-    Source: "https://github.com/hakichris/Christian_Portfolio.github.io.git",
+      'https://hakichris.github.io/Christian_Portfolio.github.io/',
+    Source: 'https://github.com/hakichris/Christian_Portfolio.github.io.git',
   },
 
   {
-    id: "4",
-    Project_name: "Uber Navigation",
-    Featured_image: "images/Snapshoot_Portfolio_(3).png",
-    Feature_image_desk: "images/Snapshoot_Portfolio_(4).png",
+    id: '4',
+    Project_name: 'Uber Navigation',
+    Featured_image: 'images/Snapshoot_Portfolio_(3).png',
+    Feature_image_desk: 'images/Snapshoot_Portfolio_(4).png',
     Featured: [
-      "CANOPY",
-      "Back End Dev",
-      "2015",
-      "Uber",
-      "Lead Developer",
-      "2018",
+      'CANOPY',
+      'Back End Dev',
+      '2015',
+      'Uber',
+      'Lead Developer',
+      '2018',
     ],
     image: [
       '<img src="images/Snapshoot Portfolio .png" alt=" Project snapshoot" />',
-      "images/Snapshoot Portfolio (4).png",
-      "images/Counter,png",
+      'images/Snapshoot Portfolio (4).png',
+      'images/Counter,png',
     ],
     Project_descr: [
-      " A daily selection of privately personalized reads; no accounts or sign-ups required.",
-      " A daily selection of privately personalized reads; no accounts orsign-ups required.",
+      ' A daily selection of privately personalized reads; no accounts or sign-ups required.',
+      ' A daily selection of privately personalized reads; no accounts orsign-ups required.',
     ],
-    technologies: ["html", "javascript", "css", "Ruby on rails"],
-    image: "images/Counter.png",
+    technologies: ['html', 'javascript', 'css', 'Ruby on rails'],
+    image: 'images/Counter.png',
     Project_descr_pop:
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent",
+      'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent',
     link_to_live_version:
-      "https://hakichris.github.io/Christian_Portfolio.github.io/",
-    Source: "https://github.com/hakichris/Christian_Portfolio.github.io.git",
+      'https://hakichris.github.io/Christian_Portfolio.github.io/',
+    Source: 'https://github.com/hakichris/Christian_Portfolio.github.io.git',
   },
 ];
 
-const works = document.querySelectorAll(".work");
-const works_desk = document.querySelectorAll(".works");
-const Model_show = document.querySelector(".model");
+const works = document.querySelectorAll('.work');
+const works_desk = document.querySelectorAll('.works');
+const Model_show = document.querySelector('.model');
 
 for (let i = 0; i < arr.length; i++) {
-  const image = document.createElement("img");
-  const images = document.createElement("img");
-  image.setAttribute("src", arr[i].Featured_image);
-  images.setAttribute("src", arr[i].Feature_image_desk);
+  const image = document.createElement('img');
+  const images = document.createElement('img');
+  image.setAttribute('src', arr[i].Featured_image);
+  images.setAttribute('src', arr[i].Feature_image_desk);
   works[i].appendChild(image);
   works_desk[i].appendChild(images);
-  works[i].querySelector(".project_name").innerHTML = arr[i].Project_name;
-  works[i].querySelector("#h3").innerHTML = arr[i].Featured[0];
-  works[i].querySelector(".langue").innerHTML = arr[i].Featured[1];
-  works[i].querySelector(".year").innerHTML = arr[i].Featured[2];
-  works[i].querySelector(".project_description").innerHTML =
+  works[i].querySelector('.project_name').innerHTML = arr[i].Project_name;
+  works[i].querySelector('#h3').innerHTML = arr[i].Featured[0];
+  works[i].querySelector('.langue').innerHTML = arr[i].Featured[1];
+  works[i].querySelector('.year').innerHTML = arr[i].Featured[2];
+  works[i].querySelector('.project_description').innerHTML =
     arr[i].Project_descr[0];
-  works[i].querySelector(".html").innerHTML = arr[i].technologies[0];
-  works[i].querySelector(".css").innerHTML = arr[i].technologies[1];
-  works[i].querySelector(".javascript").innerHTML = arr[i].technologies[2];
+  works[i].querySelector('.html').innerHTML = arr[i].technologies[0];
+  works[i].querySelector('.css').innerHTML = arr[i].technologies[1];
+  works[i].querySelector('.javascript').innerHTML = arr[i].technologies[2];
 
   //desktop
-  works_desk[i].querySelector(".project_name").innerHTML = arr[i].Project_name;
-  works_desk[i].querySelector("#h3").innerHTML = arr[i].Featured[0];
-  works_desk[i].querySelector(".langue").innerHTML = arr[i].Featured[1];
-  works_desk[i].querySelector(".year").innerHTML = arr[i].Featured[2];
-  works_desk[i].querySelector(".project_description").innerHTML =
+  works_desk[i].querySelector('.project_name').innerHTML = arr[i].Project_name;
+  works_desk[i].querySelector('#h3').innerHTML = arr[i].Featured[0];
+  works_desk[i].querySelector('.langue').innerHTML = arr[i].Featured[1];
+  works_desk[i].querySelector('.year').innerHTML = arr[i].Featured[2];
+  works_desk[i].querySelector('.project_description').innerHTML =
     arr[i].Project_descr[0];
-  works_desk[i].querySelector(".html").innerHTML = arr[i].technologies[0];
-  works_desk[i].querySelector(".css").innerHTML = arr[i].technologies[1];
-  works_desk[i].querySelector(".javascript").innerHTML = arr[i].technologies[2];
+  works_desk[i].querySelector('.html').innerHTML = arr[i].technologies[0];
+  works_desk[i].querySelector('.css').innerHTML = arr[i].technologies[1];
+  works_desk[i].querySelector('.javascript').innerHTML = arr[i].technologies[2];
 }
 
-const popup_show = document.querySelectorAll(".link_to_project");
+const popup_show = document.querySelectorAll('.link_to_project');
 
-const image = document.createElement("img");
-image.setAttribute("src", "images/Enabled.png");
-image.setAttribute("class", "close");
+const image = document.createElement('img');
+image.setAttribute('src', 'images/Enabled.png');
+image.setAttribute('class', 'close');
 
 for (const box of popup_show) {
-  box.addEventListener("click", function onClick() {
+  box.addEventListener('click', function onClick() {
     openPop();
   });
 }
 
 function openPop() {
-  const model = document.querySelector(".model");
-  const overlay = document.createElement("div");
-  overlay.classList.add("overlay");
+  const model = document.querySelector('.model');
+  const overlay = document.createElement('div');
+  overlay.classList.add('overlay');
 
   popup_show.forEach((pop) => {
     const projectId = pop.id;
@@ -228,21 +228,21 @@ function openPop() {
                       `;
 
         model.appendChild(overlay);
-        model.style.display = "block";
-        const modalImage = document.querySelector(".overlayImage");
+        model.style.display = 'block';
+        const modalImage = document.querySelector('.overlayImage');
         modalImage.appendChild(image);
         console.log(image, modalImage);
       }
     }
   });
-  const close_popup = document.querySelector(".close");
-  close_popup.addEventListener("click", function () {
+  const close_popup = document.querySelector('.close');
+  close_popup.addEventListener('click', function () {
     console.log(image);
     closePop();
   });
 }
 
 function closePop() {
-  const popup_close = document.querySelector(".model");
-  popup_close.style.display = "none";
+  const popup_close = document.querySelector('.model');
+  popup_close.style.display = 'none';
 }
